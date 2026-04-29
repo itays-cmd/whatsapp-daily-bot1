c5b8b6cf82c30193a7fcc74e9af7571a5900f810
itays-cmd <itays@odem.pro>
2026-04-29 17:26:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 import json
 
 # שנה את הפרמטרים האלו לנתונים שלך
-ID_INSTANCE = "1101XXXXXX"
-API_TOKEN = "your_token_here"
+ID_INSTANCE = "7107604629"
+API_TOKEN = "8fd010ad67b1446187956bcb2ef4b5b9bb6d963371bf447f8a"
 # עבור קבוצה ה-ID נראה כך: 120363XXXXXXXX@g.us
 # עבור מספר פרטי זה נראה כך: 97250XXXXXXX@c.us
-CHAT_ID = "120363XXXXXXXX@g.us" 
+CHAT_ID = "CkwyRjF2nt6GzMPBzCN6ZJ@g.us" 
 
 url = f"https://api.green-api.com/waInstance{ID_INSTANCE}/sendMessage/{API_TOKEN}"
 
